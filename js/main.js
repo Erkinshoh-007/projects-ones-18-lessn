bdd9a26fa113c79894fbc7ca4baed294349a3018
erkinshox <shaymurodoverkinshoh@gmail.com>
2024-06-21 10:31:42 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const sidebar = document.querySelector("nav");
+const sidebarToggle = document.querySelector(".hamburger");
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+if (innerWidth <= 1000) {
+  sidebar.classList.add("close");
+} else {
+  sidebar.classList.remove("close");
+}
